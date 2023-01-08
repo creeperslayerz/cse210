a6e5e9585feb117e4b90f664a0493281fc355048
Benski <day22004@byui.edu>
2023-01-07 18:43:14 -0700

    Finished Core Requirements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What was your grade percentage? ");
+        string gradePercentageString = Console.ReadLine();
+        int gradePercentageNumber = int.Parse(gradePercentageString);
+
+        string letter = "";
+
+        if (gradePercentageNumber >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentageNumber >= 80 && gradePercentageNumber < 90)
+        {
+            letter = "B";
+        }
+        else if (gradePercentageNumber >= 70 && gradePercentageNumber < 80)
+        {
+            letter = "C";
+        }
+        else if (gradePercentageNumber >= 60 && gradePercentageNumber < 70)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (gradePercentageNumber >= 70)
+        {
+            Console.WriteLine("Congratulations!");
+        }
+        else if (gradePercentageNumber < 70)
+        {
+            Console.WriteLine("Repetition is the key to learning!");
+        }
     }
 }
