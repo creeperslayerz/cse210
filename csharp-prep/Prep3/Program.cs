5c7cf26583fb40756486106bd1a18756901600bc
Benski <day22004@byui.edu>
2023-01-09 20:39:57 -0700

    Completed Core Challenges

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int answer = randomGenerator.Next(1,100);
+        
+        // Console.Write("What is the magic number? ");
+        // string answerStr = Console.ReadLine();
+        // int answer = int.Parse(answerStr);
+        
+        string cont = "yes";
+        
+        while (cont == "yes")
+        {
+            Console.Write("What is your guess? ");
+            string guessStr = Console.ReadLine();
+            int guess = int.Parse(guessStr);
+            
+            if (guess > answer)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < answer)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                cont = "no";
+            }
+        }
+        
     }
 }
