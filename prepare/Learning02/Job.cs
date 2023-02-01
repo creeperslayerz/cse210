8baa0774f16b940c4ec17aa40998675ceb591dff
Benski <day22004@byui.edu>
2023-02-01 07:18:05 -0700

    Took out redundancy

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -6,10 +6,6 @@ public class Job
     public string _jobTitle = "";
     public string _startYear = "";
     public string _endYear = "";
-
-    public Job()
-    {
-    }
     
     public void DisplayJobDetails()
     {
