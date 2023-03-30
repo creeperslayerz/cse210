9d817cb5a40667a6dd78977133a74350f5decb13
Benski <day22004@byui.edu>
2023-03-29 18:36:10 -0600

    Kydra player turn functional

--- sandbox/Sandbox/Boss.cs
+++ sandbox/Sandbox/Boss.cs
@@ -19,21 +19,41 @@ public abstract class Boss
     {
         return _hitPoints;
     }
+    public void SetHitPoints(int hitPoints) 
+    {
+        _hitPoints = hitPoints;
+    }
     public int GetLifeEnergy()
     {
         return _lifeEnergy;
+    }
+    public void SetLifeEnergy(int lifeEnergy) 
+    {
+        _lifeEnergy= lifeEnergy;
     }
     public int GetFireEnergy()
     {
         return _fireEnergy;
     }
+    public void SetFireEnergy(int fireEnergy) 
+    {
+        _fireEnergy= fireEnergy;
+    }
     public int GetAquaEnergy()
     {
         return _aquaEnergy;
     }
+    public void SetAquaEnergy(int aquaEnergy) 
+    {
+        _aquaEnergy= aquaEnergy;
+    }
     public int GetPrismaEnergy()
     {
         return _prismaEnergy;
+    }
+    public void SetPrismaEnergy(int prismaEnergy) 
+    {
+        _prismaEnergy= prismaEnergy;
     }
 
     public virtual void BlockAttack()
@@ -42,6 +62,6 @@ public abstract class Boss
     }
     public abstract void Attack();
     public abstract void SpecialAttack();
-
+    public abstract void PlayersTurn();
 
 }
--- sandbox/Sandbox/Kydra.cs
+++ sandbox/Sandbox/Kydra.cs
@@ -19,21 +19,41 @@ public class Kydra : Boss
     {
         return _blueHeadHP;
     }
+    public void SetBlueHeadHP(int blueHeadHP) 
+    {
+        _blueHeadHP= blueHeadHP;
+    }
     public int GetGreenHeadHP()
     {
         return _greenHeadHP;
+    }
+    public void SetGreenHeadHP(int greenHeadHP) 
+    {
+        _greenHeadHP= greenHeadHP;
     }
     public int GetRedHeadHP()
     {
         return _redHeadHP;
     }
+    public void SetRedHeadHP(int redHeadHP) 
+    {
+        _redHeadHP= redHeadHP;
+    }
     public string GetLastHeadAttacked()
     {
         return _lastHeadAttacked;
     }
+    public void SetLastHeadAttacked(string lastHeadAttacked) 
+    {
+        _lastHeadAttacked= lastHeadAttacked;
+    }
     public int GetPurpleEnergy()
     {
         return _purpleEnergy;
+    }
+    public void SetPurpleEnergy(int purpleEnergy) 
+    {
+        _purpleEnergy= purpleEnergy;
     }
 
     public override void BlockAttack()
@@ -52,9 +72,48 @@ public class Kydra : Boss
         //Red Head's attack: 10 damage for each _fireEnergy to enemy with hightest HP in range. 
         //Blue Head's attack: uses _purpleEnergy for Standard Attacks. Hits all enemy characters 10 for each _purpleEnergy
     }
-    public void CharacterTurn()
+    public override void PlayersTurn()
     {
-        //Add how much Prisma Energy was spent to Kydra's total _prismaEnergy
+        string playersTurn = "";
+        while(playersTurn != "4")
+        {
+            Console.WriteLine();
+            Console.WriteLine("1. Spend Prisma");
+            Console.WriteLine("2. Attack");
+            Console.WriteLine("3. Heal");
+            Console.WriteLine("4. End Turn");
+            Console.WriteLine("9. Concede Defeat");
+            Console.Write("What did you do on your turn? ");
+            playersTurn = Console.ReadLine();
+            if(playersTurn == "1")
+            {
+                Console.Write("How much Prisma did you spend? ");
+                string spentPrismaAsString = Console.ReadLine();
+                int spentPrisma = int.Parse(spentPrismaAsString);
+                int totalPrisma = _prismaEnergy + spentPrisma;
+                SetPrismaEnergy(totalPrisma);
+            }
+            else if(playersTurn == "2")
+            {
+                Console.Write("How much damage did you do? ");
+                string damageAsString = Console.ReadLine();
+                int damage = int.Parse(damageAsString);
+                int remainingHP = _hitPoints - damage;
+                SetPrismaEnergy(remainingHP);
+            }
+            else if(playersTurn == "3")
+            {
+                Console.WriteLine("Wise move. Kydra will not show mercy.");
+            }
+            else if(playersTurn == "9")
+            {
+                Console.WriteLine("Better luck next time. Round up some more heroes and try again!"); //TODO: Make program end here.
+            }
+            else
+            {
+                Console.WriteLine("Please choose a valid option");
+            }
+        }
     }
     public void BossTurn()
     {
